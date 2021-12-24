ddb8c9071bb63d03ac1a70dfc75354bc5a3d1ee6
JadenLeake333 <jayman425@gmail.com>
2021-12-24 00:46:52 -0500

    Restyled /search page

--- navbar.py
+++ navbar.py
@@ -10,6 +10,6 @@ def create_navbar():
     for nav in endpoints.keys():
       navbar += '<a class="nav-link fs-2" href="%s">%s</a>' %(endpoints[nav], nav)
 
-  navbar += '</nav><div class="input-group mb-3"><input type="text" id="search" class="form-control" aria-describedby="button-addon2"><button class="btn btn-outline-secondary" type="button" id="button-addon2" onclick="makeSearch()">Search</button></div></header>'
+  navbar += '</nav><div class="input-group mb-3"><input placeholder="Search Spotify for a song or playlist!" type="text" id="search" class="form-control" aria-describedby="button-addon2"><button class="btn btn-outline-secondary" type="button" id="button-addon2" onclick="makeSearch()">Search</button></div></header>'
   return navbar
 
